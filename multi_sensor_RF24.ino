0c617797bddde02b73f2d03056981580790d55f5
ONKAR-SURFACE\onkar <onkargulavani@live.com>
2019-06-26 22:44:46 -0400

    Tested update, pushing working code

--- multi_sensor_RF24.ino
+++ multi_sensor_RF24.ino
@@ -5,9 +5,8 @@
 #include <Vcc.h>
 #include <avr/wdt.h>
 #include <DHT.h>
-//#include <DHT_U.h>
 
-//#define debug                               // comment this to remove serial prints
+#define debug                               // comment this to remove serial prints
 //----------------------------------------------------------------------------------------------------
 const uint64_t pipeAddress = 0xB00B1E50C3LL;// Create pipe address for the network, "LL" is for LongLong type
 const uint8_t rfChannel = 89;               // Set channel frequency default (chan 84 is 2.484GHz to 2.489GHz)
@@ -18,14 +17,15 @@ const uint8_t CE_pin = 8;                   // This pin is used to set the nRF24
 const uint8_t CSN_pin = 9;                  // This pin is used for SPI comm chip select
 #define       LDR_pin   A0                  // LDR pin, connected to A0 analog pin
 const uint8_t DHT_pin = 7;                  // DHT 11 sensor pin
+const uint8_t PIR_pin = 2;                  // PIR pin
 
 const float VccMin   = 1.8;                 // Minimum expected Vcc level, in Volts. (0%)
 const float VccMax   = 3.2;                 // Maximum expected Vcc level, in Volts. (100%)
 const float VccCorrection = 1.0/1.0;        // Measured Vcc by multimeter divided by reported Vcc
 
-const int sleepDuration = 450;              // Sleep duration to report battery (8 Sec x number of times)(10800 for a day)
+const int sleepDuration = 2;              // Sleep duration to report battery (8 Sec x number of times)(10800 for a day)
 volatile int sleepCounter = 0;              // Counter to keep sleep count
-
+volatile bool motionDetected = false;       // motion detection status final value
 //----------------------------------------------------------------------------------------------------
 const int SENSORTYPE   = 0;
 const int SENSORNUMBER = 1;
@@ -56,14 +56,27 @@ void setup() {
   send_payload[HUMIDITY]     = (char) 0;        // Humidity
   send_payload[LIGHT]        = (char) 0;        // Light intensity (0 - 100)
 
+  pinMode(PIR_pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PIR_pin), motionDetection_ISR, CHANGE);
+  
   #ifdef debug
     Serial.begin(115200);                       //serial port to display received data
     Serial.println("Multi sensor module is online...");
   #endif
 
+  dht.begin();
   readBattery();
   readTempHumidity();
   sendData();                                   // and send to server
+}
+
+//----------------------------------------------------------------------------------------------------
+void motionDetection_ISR()
+{
+  if (false == motionDetected)
+  {
+    motionDetected = true;
+  }
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -91,11 +104,23 @@ void readLightIntensity()
 {
   int lightValue = analogRead(LDR_pin);
   send_payload[LIGHT] = (char)(map(lightValue, 0, 100, 0, 1023));
+  #ifdef debug
+    Serial.print("Light intensity:");
+    Serial.println(map(lightValue, 0, 100, 0, 1023));
+    delay(200);
+    yield();
+  #endif
 }
 //----------------------------------------------------------------------------------------------------
 void readBattery()
 {
   send_payload[BATTERY] = (char) vcc.Read_Perc(VccMin, VccMax);
+  #ifdef debug
+    Serial.print("Battery percentage:");
+    Serial.println(vcc.Read_Perc(VccMin, VccMax));
+    delay(200);
+    yield();
+  #endif
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -153,7 +178,7 @@ void loop() {
   if(sleepCounter > sleepDuration)
   {
     #ifdef debug
-      Serial.println("Sending battery status...");
+      Serial.println("Sending data...");
       delay(200);
       yield();
     #endif
@@ -166,6 +191,11 @@ void loop() {
   }
   else
   {
+    #ifdef debug
+      Serial.println("Going to sleep...");
+      delay(200);
+      yield();
+    #endif
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   }
   sleepCounter++;
